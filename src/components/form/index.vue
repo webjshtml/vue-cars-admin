376a39239812b71ea613039b3c72cc57c6bf6168
bigbigtime <409019683@qq.com>
2020-08-05 23:44:54 +0800

    lesson35-2

--- src/components/form/index.vue
+++ src/components/form/index.vue
@@ -1,12 +1,12 @@
 <template>
-    <el-form ref="form" :model="form" :label-width="labelWidth">
+    <el-form ref="form" :model="formData" :label-width="labelWidth">
         <el-form-item v-for="item in formItme" :key="item.prop" :label="item.label" :prop="item.prop" :rules="item.rules">
             <!-- Input-->
-            <el-input v-if="item.type === 'Input'" v-model.trim="form[item.prop]" :placeholder="item.placeholder" :style="{width: item.width}" :disabled="item.disabled"></el-input>
+            <el-input v-if="item.type === 'Input'" v-model.trim="formData[item.prop]" :placeholder="item.placeholder" :style="{width: item.width}" :disabled="item.disabled"></el-input>
             <!-- 省市区 -->
             <slot v-if="item.type === 'Slot'" :name="item.slotName" />
             <!-- 省市区 -->
-            <el-radio-group v-if="item.type === 'Radio'" v-model="form[item.prop]">
+            <el-radio-group v-if="item.type === 'Radio'" v-model="formData[item.prop]">
                 <el-radio v-for="radio in item.options" :label="radio.value" :key="radio.value">{{ radio.label }}</el-radio>
             </el-radio-group>
         </el-form-item>
@@ -29,6 +29,10 @@ export default {
             type: String,
             default: "120px"
         },
+        formData: {
+            type: Object,
+            default: () => {}
+        },
         formItme: {
             type: Array,
             default: () => []
@@ -41,7 +45,6 @@ export default {
     },
     data(){
         return {
-            form: {},
             type_msg: {
                 "Input": "请输入",
                 "Radio": "请选择"
@@ -50,15 +53,12 @@ export default {
     },
     methods: {
         initFormData(){
-            const formData = {};
             this.formItme.forEach(item => {
-                if(item.prop) { formData[item.prop] = item.value || null; }
                 // rules 规则
                 if(item.required) { this.rules(item); }
                 // 自定义检验规则
                 if(item.validator) { item.rules = item.validator; }
             })
-            this.form = formData;
         },
         rules(item){
             const requiredRules = [{ required: true, message: item.required_msg || `${this.type_msg[item.type]}${item.label}`, trigger: 'change' }];
